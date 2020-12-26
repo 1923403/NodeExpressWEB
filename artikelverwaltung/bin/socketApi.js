2efa2589700e84a50b941d631f0efddf040713f4
1923403 <steffen.hausmann@outlook.com>
2020-12-26 17:31:11 +0100

    datenaustausch ueberarbeitet

--- artikelverwaltung/bin/socketApi.js
+++ artikelverwaltung/bin/socketApi.js
@@ -5,6 +5,9 @@ const path = require("path");
 const fs = require("fs");
 socketAPI.io = io;
 
+let artikelId = 0;
+let listenId = 0;
+
 io.on("connection", (socket) => {
   socket.on("artikel", (artikel) => {
     console.log("artikel angekommen: ");
@@ -12,8 +15,60 @@ io.on("connection", (socket) => {
     let rohdaten = fs.readFileSync(
       path.join(__dirname, "..", "data", "listen.json")
     );
-    console.log(JSON.parse(rohdaten));
+    parsedData = JSON.parse(artikel);
+    if (parsedData["id"] === "") {
+      // UEBERARBEITEN!!!!!!!!!!!!!!!!!
+      // parsedData["id"] = Math.floor(Math.random() * 10000);
+      parsedData["id"] = artikelId;
+    } else {
+      console.log("suche id");
+      let artikelId2 = parsedData["id"];
+      console.log(`artikelID2: ${artikelId2}`);
+      console.log(rohdaten);
+      //rohdaten fehlerhaft...überarbeiten...
+      // rohdaten = JSON.parse(rohdaten);
+      console.log(rohdaten);
+      sucheArtikel(rohdaten, artikelId2);
+    }
+    console.log(parsedData["name"]);
+
+    // jsonAusgeben(JSON.parse(artikel));
+    // artikelliste = JSON.parse(rohdaten);
+    // console.log(artikelliste);
+    // artikelliste2 = JSON.stringify(artikelliste);
+    // listenSpeichern(artikelliste2);
   });
 });
+
+function sucheArtikel(json, id) {
+  console.log(typeof json);
+  Object.entries(json).forEach(([key, value]) => {
+    console.log(key);
+    if (key === "id") {
+      console.log("key gefunden...");
+      if (value === id) {
+        console.log("ID vorhanden");
+      }
+    }
+  });
+}
+
+function listenSpeichern(daten) {
+  fs.writeFile(
+    path.join(__dirname, "..", "data", "listen2.json"),
+    daten,
+    (err) => {
+      if (err) throw err;
+      console.log("Artikellisten wurden gespeichert...");
+    }
+  );
+}
+
+function jsonAusgeben(json) {
+  console.log(typeof json);
+  Object.entries(json).forEach(([key, value]) => {
+    console.log(`${key} ${value}`);
+  });
+}
 
 module.exports = socketAPI;

3ec03d47b8fa5dc387f0de3ac91e75a5141487ea
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-12-26 12:38:33 +0100

    html --> pug

---
+++ artikelverwaltung/routes/artikeluebersicht.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const path = require("path");
+var router = express.Router();
+
+/* GET artikeluebersicht page. */
+router.get('/', (req, res) => { 
+    var data;
+    res.render('artikeluebersicht', {name1:"test1", name2: "test2"});
+  //res.sendFile(path.join(__dirname, "..", "public", "artikeluebersicht.html"));
+  //res.sendFile(path.join(__dirname, "..", "data", "listen.json"));
+});
+
+router.get('/data', (req, res)=>{
+
+});
+module.exports = router;

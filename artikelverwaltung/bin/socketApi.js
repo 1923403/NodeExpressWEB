c57f9a70c6d9f1e1b1de21e924198d6f354bfc16
1923403 <steffen.hausmann@outlook.com>
2020-12-29 15:33:07 +0100

    problem mit leerer liste behoben

--- artikelverwaltung/bin/socketApi.js
+++ artikelverwaltung/bin/socketApi.js
@@ -6,56 +6,68 @@ const fs = require("fs");
 socketAPI.io = io;
 
 io.on("connection", (socket) => {
-  sendeArtikelliste(socket);
+  socket.emit("artikelListe", holeBestandsdaten());
+  // sendeArtikelliste(socket);
+
   socket.on("artikel", (artikel) => {
-    const neueArtikeldaten = JSON.parse(artikel);
-    const bestand = holeBestandsdaten();
-    let artikelId = holeLetzteId(bestand);
-    if (neueArtikeldaten["id"] === "") {
-      neueArtikeldaten["id"] = ++artikelId;
-      bestand.push(neueArtikeldaten);
-      aktualisiereBestandsliste(bestand);
-    } else {
-      artikelAendern(neueArtikeldaten, bestand);
-    }
+    verarbeiteNeueDaten(artikel);
+    // const neueArtikeldaten = JSON.parse(artikel);
+    // const bestand = holeBestandsdaten();
+    // let artikelId = holeLetzteId(bestand);
+    // if (neueArtikeldaten["id"] === "") {
+    //   neueArtikeldaten["id"] = ++artikelId;
+    //   bestand.push(neueArtikeldaten);
+    //   aktualisiereBestandsliste(bestand);
+    // } else {
+    //   artikelAendern(neueArtikeldaten, bestand);
+    // }
   });
 
   socket.on("artikelLoeschen", (id) => {
-    console.log("artikel löschen");
-    const bestand = holeBestandsdaten();
-    loescheArtikel(bestand, id);
+    loescheArtikel(id);
+
     io.emit("artikelLoeschen", id);
   });
 });
 
-function artikelAendern(artikel, bestand) {
-  for (let i = 0; i < bestand.length; i++) {
-    if (bestand[i]["id"] == artikel["id"]) {
-      bestand[i]["name"] = artikel["name"];
-      bestand[i]["hersteller"] = artikel["hersteller"];
-      bestand[i]["einkaufspreis"] = artikel["einkaufspreis"];
-      bestand[i]["verkaufspreis"] = artikel["verkaufspreis"];
-      bestand[i]["kategorie"] = artikel["kategorie"];
-      bestand[i]["stueckzahl"] = artikel["stueckzahl"];
-      console.log("id gefunden...updating...");
-      break;
-    }
+// function sendeArtikelliste(socket) {
+//   socket.emit("artikelListe", holeBestandsdaten());
+// }
+
+function holeBestandsdaten() {
+  const rohdaten = fs.readFileSync(
+    path.join(__dirname, "..", "data", "listen.json")
+  );
+
+  return JSON.parse(rohdaten);
+}
+
+function verarbeiteNeueDaten(artikel) {
+  const neueArtikeldaten = JSON.parse(artikel);
+  const bestand = holeBestandsdaten();
+  let artikelId;
+
+  bestand == "" ? (artikelId = 0) : (artikelId = holeLetzteId(bestand));
+
+  if (neueArtikeldaten["id"] === "") {
+    neueArtikeldaten["id"] = ++artikelId;
+    bestand.push(neueArtikeldaten);
+    aktualisiereBestandsliste(bestand);
+  } else {
+    artikelAendern(neueArtikeldaten, bestand);
   }
-  aktualisiereBestandsliste(bestand);
 }
 
-function sendeArtikelliste(socket) {
-  socket.emit("artikelListe", holeBestandsdaten());
-}
+function loescheArtikel(id) {
+  const bestand = holeBestandsdaten();
 
-function loescheArtikel(bestand, id) {
-  console.log("lösche" + id);
   for (let i = 0; i < bestand.length; i++) {
     if (bestand[i]["id"] == id) {
       bestand.splice(i, 1);
       break;
     }
   }
+
   aktualisiereBestandsliste(bestand);
 }
 
@@ -63,11 +75,34 @@ function holeLetzteId(bestand) {
   return bestand[bestand.length - 1]["id"];
 }
 
-function holeBestandsdaten() {
-  const rohdaten = fs.readFileSync(
-    path.join(__dirname, "..", "data", "listen.json")
-  );
-  return JSON.parse(rohdaten);
+function artikelAendern(artikel, bestand) {
+  const suchbegriffe = [
+    "id",
+    "name",
+    "hersteller",
+    "einkaufspreis",
+    "verkaufspreis",
+    "kategorie",
+    "stueckzahl",
+  ];
+
+  for (let i = 0; i < bestand.length; i++) {
+    if (bestand[i]["id"] == artikel["id"]) {
+      suchbegriffe.forEach((element) => {
+        bestand[i][`${element}`] = artikel[`${element}`];
+      });
+
+      // bestand[i]["name"] = artikel["name"];
+      // bestand[i]["hersteller"] = artikel["hersteller"];
+      // bestand[i]["einkaufspreis"] = artikel["einkaufspreis"];
+      // bestand[i]["verkaufspreis"] = artikel["verkaufspreis"];
+      // bestand[i]["kategorie"] = artikel["kategorie"];
+      // bestand[i]["stueckzahl"] = artikel["stueckzahl"];
+      // console.log("id gefunden...updating...");
+      break;
+    }
+  }
+  aktualisiereBestandsliste(bestand);
 }
 
 function aktualisiereBestandsliste(bestandsliste) {
